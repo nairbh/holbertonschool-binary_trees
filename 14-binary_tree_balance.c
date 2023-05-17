9e15912b16064f0a82a5269a7682296c9fcc1f5c
nairbh <houcinenair@proton.me>
2023-05-17 11:10:28 +0200

    fix error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,3 +15,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height of the tree or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
+	return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
