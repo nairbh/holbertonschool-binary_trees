2fb21e78505609f0bfa792acef5f5fa97bf7cc0f
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-05-19 12:27:30 +0200

    fix linter warnings

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,10 +7,10 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 if (node == NULL)
-  return (NULL);
+return (NULL);
 else if (node->parent == NULL || node->parent->parent == NULL)
-  return (NULL);
+return (NULL);
 else if (node->parent->parent->left == node->parent)
-  return (node->parent->parent->right);
+return (node->parent->parent->right);
 return (node->parent->parent->left);
 }
